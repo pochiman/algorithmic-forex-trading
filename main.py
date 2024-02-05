b8cc35aff8053961037e7d12598e2859cab9fcec
pochiman <pochiman@yahoo.com>
2024-02-05 11:35:51 -0500

    initial commit

--- main.py
+++ main.py
@@ -4,12 +4,6 @@ from infrastructure.instrument_collection import instrumentCollection
 if __name__ == '__main__':
     api = OandaApi()
 
-    # data = api.get_instruments()
-    # [print(x['name']) for x in data]
-
-    # data = api.get_account_summary()
-    # print(data)
-
     instrumentCollection.CreateFile(api.get_account_instruments(), "./data")
     instrumentCollection.LoadInstruments("./data")
     instrumentCollection.PrintInstruments()

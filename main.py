c12d38914d4dff01f1a52a4438230e22b26cc6ef
pochiman <pochiman@yahoo.com>
2024-02-07 09:04:04 -0500

    initial commit

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from api.oanda_api import OandaApi
 from infrastructure.instrument_collection import instrumentCollection
-
+from simulation.ma_cross import run_ma_sim
 if __name__ == '__main__':
-    api = OandaApi()
+    # api = OandaApi()
 
-    instrumentCollection.CreateFile(api.get_account_instruments(), "./data")
-    instrumentCollection.LoadInstruments("./data")
-    instrumentCollection.PrintInstruments()
+    # instrumentCollection.CreateFile(api.get_account_instruments(), "./data")
+    # instrumentCollection.LoadInstruments("./data")
+    # instrumentCollection.PrintInstruments()
+    run_ma_sim(curr_list=["EUR", "USD", "AUD", "GBP"])

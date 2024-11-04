bc47e6c126eda50b6991376c1f2dd0de2d434865
pochiman <pochiman@yahoo.com>
2024-11-04 10:05:43 -0500

    initial commit

---
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,15 @@
+from api.oanda_api import OandaApi
+from infrastructure.instrument_collection import instrumentCollection
+
+if __name__ == '__main__':
+    api = OandaApi()
+
+    # data = api.get_instruments()
+    # [print(x['name']) for x in data]
+
+    # data = api.get_account_summary()
+    # print(data)
+
+    instrumentCollection.CreateFile(api.get_account_instruments(), "./data")
+    instrumentCollection.LoadInstruments("./data")
+    instrumentCollection.PrintInstruments()

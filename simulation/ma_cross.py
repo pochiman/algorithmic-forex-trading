aaf81adb408b1b09754424ab1d761b41b0842f7f
pochiman <pochiman@yahoo.com>
2024-11-04 22:17:57 -0500

    initial commit

--- simulation/ma_cross.py
+++ simulation/ma_cross.py
@@ -24,10 +24,6 @@ class MAResult:
             ma_s = self.ma_s
         )
 
-
-
-
-
 BUY = 1
 SELL = -1
 NONE = 0
@@ -54,8 +50,7 @@ def get_trades(df_analysis, instrument):
     df_trades.fillna(0, inplace=True)
     df_trades["GAIN"] = df_trades.DIFF / instrument.pipLocation
     df_trades["GAIN"] = df_trades["GAIN"] * df_trades["TRADE"]
-    total_gain = df_trades["GAIN"].sum()
-    return dict(total_gain=int(total_gain), df_trades=df_trades)
+    return df_trades
 
 
 def assess_pair(price_data, ma_l, ma_s, instrument):
@@ -63,9 +58,13 @@ def assess_pair(price_data, ma_l, ma_s, instrument):
     df_analysis["DELTA"] = df_analysis[ma_s] - df_analysis[ma_l]
     df_analysis["DELTA_PREV"] = df_analysis["DELTA"].shift(1)
     df_analysis["TRADE"] = df_analysis.apply(is_trade, axis=1)
-    # print(instrument.name, ma_l, ma_s)
-    # print(df_analysis.head(3))
-    return get_trades(df_analysis, instrument)
+    df_trades = get_trades(df_analysis, instrument)
+    return MAResult(
+        df_trades,
+        instrument.name,
+        ma_l,
+        ma_s
+    )
 
 def analyse_pair(instrument, granularity, ma_long, ma_short):
 
@@ -76,22 +75,22 @@ def analyse_pair(instrument, granularity, ma_long, ma_short):
     # print(pair)
     # print(price_data.head(3))
 
+    results_list = []
+
     for ma_l in ma_long:
         for ma_s in ma_short:
             if ma_l <= ma_s:
                 continue
 
-            result = assess_pair(
+            ma_result = assess_pair(
                 price_data,
                 get_ma_col(ma_l),
                 get_ma_col(ma_s),
                 instrument
             )
-
-            tg = result['total_gain']
-            nt = result['df_trades'].shape[0]
-
-            print(f"{pair} {granularity} {ma_s}-{ma_l} nt:{nt} tg:{tg}")
+            print(ma_result)
+            results_list.append(ma_result)
+    # create df
 
 
 

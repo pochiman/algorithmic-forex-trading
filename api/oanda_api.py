349314285905485661cd8e7abb046feb5224c0b7
pochiman <pochiman@yahoo.com>
2024-11-09 23:42:23 -0500

    initial commit

--- api/oanda_api.py
+++ api/oanda_api.py
@@ -1,6 +1,9 @@
 import ssl
 import requests
+import pandas as pd
 import constants.defs as defs
+from dateutil import parser
+from datetime import datetime as dt
 
 class OandaApi:
 
@@ -44,3 +47,47 @@ class OandaApi:
     
     def get_account_instruments(self):
         return self.get_account_ep("instruments", "instruments")
+
+    def fetch_candles(self, pair_name, count=10, granularity="H1",
+                            price="MBA", date_f=None, date_t=None):
+        url = f"instruments/{pair_name}/candles"
+        params = dict(
+            granularity = granularity,
+            price = price
+        )
+
+        if date_f is not None and date_t is not None:
+            date_format = "%Y-%m-%dT%H:%M:%SZ"
+            params["from"] = dt.strftime(date_f, date_format)
+            params["to"] = dt.strftime(date_t, date_format)
+        else:
+            params["count"] = count
+
+        ok, data = self.make_request(url, params=params)
+        
+        if ok == True and 'candles' in data:
+            return data['candles']
+        else:
+            print("ERROR fetch_candles()", params, data)
+            return None
+
+    def get_candles_df(self, data):
+        if len(data) == 0:
+            return pd.DataFrame.empty
+        
+        prices = ['mid', 'bid', 'ask']
+        ohlc = ['o', 'h', 'l', 'c']
+        
+        final_data = []
+        for candle in data:
+            if candle['complete'] == False:
+                continue
+            new_dict = {}
+            new_dict['time'] = parser.parse(candle['time'])
+            new_dict['volume'] = candle['volume']
+            for p in prices:
+                for o in ohlc:
+                    new_dict[f"{p}_{o}"] = float(candle[p][o])
+            final_data.append(new_dict)
+        df = pd.DataFrame.from_dict(final_data)
+        return df
--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from api.oanda_api import OandaApi
 from infrastructure.instrument_collection import instrumentCollection
 from simulation.ma_cross import run_ma_sim
 if __name__ == '__main__':
-    # api = OandaApi()
+    api = OandaApi()
+    print(api.fetch_candles("EUR_USD", granularity="D", price="MB"))
 
     # instrumentCollection.CreateFile(api.get_account_instruments(), "./data")
     # instrumentCollection.LoadInstruments("./data")
     # instrumentCollection.PrintInstruments()
-    run_ma_sim(curr_list=["EUR", "USD", "GBP", "JPY", "AUD", "CAD"])
+    # run_ma_sim(curr_list=["EUR", "USD", "GBP", "JPY", "AUD", "CAD"])

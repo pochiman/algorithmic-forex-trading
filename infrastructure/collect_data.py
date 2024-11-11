57212fd3d1b77453165261b5aad6ef6af08a2f47
pochiman <pochiman@yahoo.com>
2024-11-11 11:14:48 -0500

    initial commit

--- infrastructure/collect_data.py
+++ infrastructure/collect_data.py
@@ -1,13 +1,13 @@
-
 from turtle import pd
 import pandas as pd
 import datetime as dt
 from dateutil import parser
+from parso import parse
 
 from infrastructure.instrument_collection import InstrumentCollection
 from api.oanda_api import OandaApi
 
-CANDLE_COUNT = 3000
+CANDLE_COUNT = 200
 
 INCREMENTS = {
     'M5' : 5 * CANDLE_COUNT,
@@ -22,7 +22,27 @@ def save_file(final_df: pd.DataFrame, file_prefix, granularity, pair):
 
 def fetch_candles(pair, granularity, date_f: dt.datetime,
                     date_t: dt.datetime, api: OandaApi):
-    return None
+    
+    attempts = 0
+
+    while attempts < 3:
+
+        candles_df = api.get_candles_df(
+            pair,
+            granularity=granularity,
+            date_f=date_f,
+            date_t=date_t
+        )
+
+        if candles_df is not None:
+            break
+
+        attempts += 1
+
+    if candles_df is not None and candles_df.empty == False:
+        return candles_df
+    else:
+        return None
 
 
 def collect_data(pair, granularity, date_f, date_t, file_prefix, api: OandaApi):
@@ -49,8 +69,9 @@ def collect_data(pair, granularity, date_f, date_t, file_prefix, api: OandaApi):
             api
         )
 
-        if candles is not None and candles.empty == False:
+        if candles is not None:
             candle_dfs.append(candles)
+            print(f"{pair} {granularity} {from_date} {to_date} --> {candles.shape[0]} candles loaded")
         else:
             print(f"{pair} {granularity} {from_date} {to_date} --> NO CANDLES")
         
@@ -61,6 +82,7 @@ def collect_data(pair, granularity, date_f, date_t, file_prefix, api: OandaApi):
         save_file(final_df, file_prefix, granularity, pair)
     else:
         print(f"{pair} {granularity} --> NO DATA SAVED!")        
+
 
 
 def run_collection(ic: InstrumentCollection, api: OandaApi):
@@ -74,8 +96,8 @@ def run_collection(ic: InstrumentCollection, api: OandaApi):
                     collect_data(
                         pair,
                         granularity,
-                        "2021-06-01T00:00:00Z",
-                        "2021-12-01T00:00:00Z",
+                        "2021-10-07T00:00:00Z",
+                        "2021-10-12T00:00:00Z",
                         "./data/",
                         api
                     )

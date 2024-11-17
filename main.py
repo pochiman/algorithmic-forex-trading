1e5fbfb03add14df3a95785db60af66e1f5cea2c
pochiman <pochiman@yahoo.com>
2024-11-17 09:39:06 -0500

    initial commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from api.oanda_api import OandaApi
 from infrastructure.instrument_collection import instrumentCollection
 from simulation.ma_cross import run_ma_sim
-from simulation.ema_macd import run_ema_macd
+from simulation.ema_macd_mp import run_processes
 from dateutil import parser
 from infrastructure.collect_data import run_collection
 
@@ -9,5 +9,5 @@ if __name__ == '__main__':
     # api = OandaApi()
     instrumentCollection.LoadInstruments("./data")
     # run_collection(instrumentCollection, api)
-    run_ema_macd(instrumentCollection)
-    
+    # run_ema_macd(instrumentCollection)
+    run_processes()

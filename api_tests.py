deb1836a056e26793da7e702891b65789a6c5aa9
pochiman <pochiman@yahoo.com>
2024-11-21 15:50:03 -0500

    initial commit

--- api_tests.py
+++ api_tests.py
@@ -2,7 +2,10 @@ from api.oanda_api import OandaApi
 from infrastructure.instrument_collection import instrumentCollection
 import time
 
+from models.candle_timing import CandleTiming
+
 if __name__ == '__main__':
     api = OandaApi()
     instrumentCollection.LoadInstruments("./data")
-    print(api.last_complete_candle("EUR_USD", granularity="M5"))
+    dd = api.last_complete_candle("EUR_USD", granularity="M5")
+    print(CandleTiming(dd))

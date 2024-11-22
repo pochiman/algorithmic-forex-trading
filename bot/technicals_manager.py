df72c048c9d59354dd7809a25433f3042489ebd5
pochiman <pochiman@yahoo.com>
2024-11-22 12:12:57 -0500

    initial commit

--- bot/technicals_manager.py
+++ bot/technicals_manager.py
@@ -1,13 +1,25 @@
 import pandas as pd
 
+from technicals.indicators import BollingerBands
+
 pd.set_option('display.max_columns', None)
 pd.set_option('expand_frame_repr', False)
 
 
 from api.oanda_api import OandaApi
 from models.trade_settings import TradeSettings
+import constants.defs as defs
 
 ADDROWS = 20
+
+def apply_signal(row, trade_settings: TradeSettings):
+
+    if row.SPREAD <= trade_settings.maxspread:
+        if row.mid_c > row.BB_UP and row.mid_o < row.BB_UP:
+            return defs.SELL
+        elif row.mid_c < row.BB_LW and row.mid_o > row.BB_LW:
+            return defs.BUY
+    return defs.NONE
 
  
 def process_candles(df: pd.DataFrame, pair, trade_settings: TradeSettings, log_message):
@@ -16,9 +28,11 @@ def process_candles(df: pd.DataFrame, pair, trade_settings: TradeSettings, log_m
     df['PAIR'] = pair
     df['SPREAD'] = df.ask_c - df.bid_c
 
-    # make indicator
+    df = BollingerBands(df, trade_settings.n_ma, trade_settings.n_std)
+    df['SIGNAL'] = df.apply(apply_signal, axis=1, trade_settings=trade_settings)
+    df['GAIN'] = abs(df.mid_c - df.BB_MA)
 
-    log_cols = ['PAIR', 'time', 'mid_c', 'mid_o', 'SPREAD']
+    log_cols = ['PAIR', 'time', 'mid_c', 'mid_o', 'SPREAD', 'GAIN', 'SIGNAL']
     log_message(f"process_candles:\n{df[log_cols].tail()}", pair)
 
 

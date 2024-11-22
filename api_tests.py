407ca68dd78453d862480c174ad0b8d53ee6c527
pochiman <pochiman@yahoo.com>
2024-11-22 17:32:23 -0500

    initial commit

--- api_tests.py
+++ api_tests.py
@@ -3,10 +3,14 @@ from infrastructure.instrument_collection import instrumentCollection
 import time
 
 from models.candle_timing import CandleTiming
+from bot.trade_risk_calculator import get_trade_units
+import constants.defs as defs
+
+def lm(msg, pair):
+    print(msg, pair)
 
 if __name__ == '__main__':
     api = OandaApi()
     instrumentCollection.LoadInstruments("./data")
-    # dd = api.last_complete_candle("EUR_USD", granularity="M5")
-    # print(CandleTiming(dd))
-    print(api.get_prices(["GBP_JPY", "AUD_NZD"]))
+
+    print(get_trade_units(api, "GBP_JPY", defs.BUY, 0.4, 60, lm))

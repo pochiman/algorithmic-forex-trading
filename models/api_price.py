943a2f839048a079587ca035d12b720c3282e6ad
pochiman <pochiman@yahoo.com>
2024-11-22 19:43:59 -0500

    initial commit

--- models/api_price.py
+++ models/api_price.py
@@ -1,3 +1,4 @@
+'''
 class ApiPrice:
 
     def __init__(self, api_ob):
@@ -9,4 +10,19 @@ class ApiPrice:
 
     def __repr__(self):
         return f"ApiPrice() {self.instrument} {self.ask} {self.bid} {self.sell_conv:.6f} {self.buy_conv:.6f}"
-    
+'''
+class ApiPrice:
+
+    def __init__(self, api_ob, homeConversions):
+        self.instrument = api_ob['instrument']
+        self.ask = float(api_ob['asks'][0]['price'])
+        self.bid = float(api_ob['bids'][0]['price'])
+
+        base_instrument = self.instrument.split('_')[1]
+        for hc in homeConversions:
+            if hc['currency'] == base_instrument:
+                self.sell_conv = float(hc['positionValue'])
+                self.buy_conv = float(hc['positionValue'])
+
+    def __repr__(self):
+        return f"ApiPrice() {self.instrument} {self.ask} {self.bid} {self.sell_conv:.6f} {self.buy_conv:.6f}"

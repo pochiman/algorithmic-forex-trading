3f44cd948155d0803f81faff7f703f823eb1ca91
pochiman <pochiman@yahoo.com>
2024-11-30 14:20:43 -0500

    initial commit

--- scraping/fx_calendar.py
+++ scraping/fx_calendar.py
@@ -1,6 +1,16 @@
 from bs4 import BeautifulSoup
 import pandas as pd
 import requests
+from dateutil import parser
+
+def get_date(c):
+    tr = c.select_one("tr")
+    ths = list(tr.select("th"))
+    for th in ths:
+        if th.has_attr("colspan"):
+            date_text = th.get_text().strip()
+            return parser.parse(date_text)
+    return None   
 
 def fx_calendar():
 
@@ -8,7 +18,7 @@ def fx_calendar():
 
     headers = {
         "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0",
-        "Cookie":"calendar-importance=3; cal-custom-range=2022-03-21 00:00|2022-03-26 00:00; TEServer=TEIIS3; cal-timezone-offset=0;"
+        "Cookie":"calendar-importance=3; cal-custom-range=2022-03-28 00:00|2022-04-02 00:00; TEServer=TEIIS3; cal-timezone-offset=0;"
     }
 
     resp = session.get("https://tradingeconomics.com/calendar", headers=headers)
@@ -17,4 +27,11 @@ def fx_calendar():
 
     table = soup.select_one("table#calendar")
 
-    print(table)
+    last_header_date = None
+
+    for c in table.children:
+        if c.name == 'thead':
+            if 'class' in c.attrs and 'hidden-head' in c.attrs['class']:
+                continue
+            last_header_date = get_date(c)
+            print(last_header_date)

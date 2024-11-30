9c9113374d36b3a05f9ecef2164dd258f7a9baad
pochiman <pochiman@yahoo.com>
2024-11-30 11:08:20 -0500

    initial commit

--- scraping/dailyfx_com.py
+++ scraping/dailyfx_com.py
@@ -6,22 +6,22 @@ def dailyfx_com():
 
     resp = requests.get("https://www.dailyfx.com/sentiment")
     
-    # print(resp.content)
-    # print(resp.status_code)
-    
     soup = BeautifulSoup(resp.content, 'html.parser')
-    
-    # print(soup)
     
     rows = soup.select(".dfx-technicalSentimentCard")
 
+    pair_data = []
+
     for r in rows:
-        print()
         card = r.select_one(".dfx-technicalSentimentCard__pairAndSignal")
-        print(card.select_one('a').get_text().replace("/", "_"))
-        print(card.select_one('span').get_text())
-        change_values = r.select(".dfx-technicalSentimentCard_changeValue")
-        print(change_values[0].get_text())
-        print(change_values[1].get_text())
-        print(change_values[3].get_text())
-        print(change_values[4].get_text())
+        change_values = r.select(".dfx-technicalSentimentCard__changeValue")
+        pair_data.append(dict(
+            pair=card.select_one('a').get_text().replace("/", "_"),
+            sentiment=card.select_one('span').get_text(),
+            longs_d=change_values[0].get_text(),
+            shorts_d=change_values[1].get_text(),
+            longs_w=change_values[3].get_text(),
+            shorts_w=change_values[4].get_text()
+        ))
+
+    return pd.DataFrame.from_dict(pair_data)

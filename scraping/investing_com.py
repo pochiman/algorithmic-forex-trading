3c5629efc6339ea5cb979d27aafdd93524e14913
pochiman <pochiman@yahoo.com>
2024-11-30 12:10:48 -0500

    initial commit

--- scraping/investing_com.py
+++ scraping/investing_com.py
@@ -18,6 +18,16 @@ def investing_com():
     resp = requests.get("https://www.investing.com/common/technical_studies/technical_studies_data.php", 
                         params=params, headers=headers)
     
-    print(resp.content)
+    # print(resp.content)
     print(resp.status_code)
-    
+
+    text = resp.content.decode("utf-8")
+
+    index_start = text.index("pair_name=")
+    index_end = text.index("*;*quote_link")
+
+    data_str = text[index_start:index_end]
+
+    split_data_str = data_str.split('*;*')
+
+    [print(x) for x in split_data_str]

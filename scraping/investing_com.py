af401ff32e8f41b87a22f4d3a210e89cd4ee7c68
pochiman <pochiman@yahoo.com>
2024-11-30 13:29:58 -0500

    initial commit

--- scraping/investing_com.py
+++ scraping/investing_com.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import pandas as pd
 import requests
+import datetime as dt
+import time
 
 data_keys = [
     'pair_name',
@@ -19,8 +21,12 @@ data_keys = [
     'percent_bearish'
 ]
 
-def get_data_object(text_list):
+def get_data_object(text_list, pair_id, time_frame):
     data = {}
+    data['pair_id'] = pair_id
+    data['time_frame'] = time_frame
+    data['updated'] = dt.datetime.utcnow()
+
     for item in text_list:
         temp_item = item.split("=")
         if len(temp_item) == 2 and temp_item[0] in data_keys:
@@ -32,7 +38,7 @@ def get_data_object(text_list):
     return data
 
 
-def investing_com():
+def investing_com_fetch(pair_id, time_frame):
 
     headers = {
         "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0"
@@ -40,15 +46,15 @@ def investing_com():
 
     params = dict(
         action='get_studies',
-        pair_ID=7,
-        time_frame=3600
+        pair_ID=pair_id,
+        time_frame=time_frame
     )
 
     resp = requests.get("https://www.investing.com/common/technical_studies/technical_studies_data.php", 
                         params=params, headers=headers)
     
     # print(resp.content)
-    print(resp.status_code)
+    # print(resp.status_code)
 
     text = resp.content.decode("utf-8")
 
@@ -57,4 +63,15 @@ def investing_com():
 
     data_str = text[index_start:index_end]
 
-    print(get_data_object(data_str.split('*;*')))
+    return get_data_object(data_str.split('*;*'), pair_id, time_frame)
+
+
+def investing_com():
+    data = []
+    for pair_id in range(1, 12):
+        for time_frame in [3600, 86400]:
+            print(pair_id, time_frame)
+            data.append(investing_com_fetch(pair_id, time_frame))
+            time.sleep(0.5)
+
+    return pd.DataFrame.from_dict(data)
--- scraping_tests.py
+++ scraping_tests.py
@@ -2,5 +2,5 @@ from scraping.dailyfx_com import dailyfx_com
 from scraping.investing_com import investing_com
 
 if __name__ == "__main__":
-    print(dailyfx_com())
-    investing_com()
+    # print(dailyfx_com())
+    print(investing_com())

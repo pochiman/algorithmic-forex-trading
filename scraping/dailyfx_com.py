ea36ba1dc54d98bac1aa738132abac3f54a91978
pochiman <pochiman@yahoo.com>
2024-11-30 10:51:42 -0500

    initial commit

--- scraping/dailyfx_com.py
+++ scraping/dailyfx_com.py
@@ -11,5 +11,17 @@ def dailyfx_com():
     
     soup = BeautifulSoup(resp.content, 'html.parser')
     
-    print(soup)
+    # print(soup)
     
+    rows = soup.select(".dfx-technicalSentimentCard")
+
+    for r in rows:
+        print()
+        card = r.select_one(".dfx-technicalSentimentCard__pairAndSignal")
+        print(card.select_one('a').get_text().replace("/", "_"))
+        print(card.select_one('span').get_text())
+        change_values = r.select(".dfx-technicalSentimentCard_changeValue")
+        print(change_values[0].get_text())
+        print(change_values[1].get_text())
+        print(change_values[3].get_text())
+        print(change_values[4].get_text())

d3f45e08dcea41f7d1997493ac2f4aacbc2ffced
pochiman <pochiman@yahoo.com>
2024-11-30 21:00:39 -0500

    initial commit

--- scraping/fx_calendar.py
+++ scraping/fx_calendar.py
@@ -2,6 +2,12 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import requests
 from dateutil import parser
+import time
+import datetime as dt
+
+
+pd.set_option("display.max_rows", None)
+
 
 def get_date(c):
     tr = c.select_one("tr")
@@ -19,6 +25,13 @@ def get_data_point(key, element):
             return d.get_text()
     return ''    
 
+
+def get_data_for_key(tr, key):
+    if tr.has_attr(key):
+        return tr.attrs[key]
+    return ''
+
+
 def get_data_dict(item_date, table_rows):
 
     data = []
@@ -26,10 +39,10 @@ def get_data_dict(item_date, table_rows):
     for tr in table_rows:
         data.append(dict(
             date=item_date,
-            country=tr.attrs['data-country'],
-            category=tr.attrs['data-category'],
-            event=tr.attrs['data-event'],
-            symbol=tr.attrs['data-symbol'],
+            country=get_data_for_key(tr, 'data-country'),
+            category=get_data_for_key(tr, 'data-category'),
+            event=get_data_for_key(tr, 'data-event'),
+            symbol=get_data_for_key(tr, 'data-symbol'),
             actual=get_data_point('actual', tr),
             previous=get_data_point('previous', tr),
             forecast=get_data_point('forecast', tr)
@@ -38,13 +51,18 @@ def get_data_dict(item_date, table_rows):
     return data
 
 
-def fx_calendar():
+def get_fx_calendar(from_date):
 
     session = requests.Session()
 
+    fr_d_str = dt.datetime.strftime(from_date, "%Y-%m-%d 00:00:00")
+
+    to_date = from_date + dt.timedelta(days=6)
+    to_d_str = dt.datetime.strftime(to_date, "%Y-%m-%d 00:00:00")
+
     headers = {
         "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0",
-        "Cookie":"calendar-importance=3; cal-custom-range=2022-03-28 00:00|2022-04-02 00:00; TEServer=TEIIS3; cal-timezone-offset=0;"
+        "Cookie":f"calendar-importance=3; cal-custom-range={fr_d_str}|{to_d_str}; TEServer=TEIIS3; cal-timezone-offset=0;"
     }
 
     resp = session.get("https://tradingeconomics.com/calendar", headers=headers)
@@ -69,4 +87,21 @@ def fx_calendar():
     for item_date, table_rows in trs.items():
         final_data += get_data_dict(item_date, table_rows)    
 
-    [print(x) for x in final_data]
+    # [print(x) for x in final_data]
+    return final_data
+
+
+def fx_calendar():
+    
+    final_data = []
+
+    start = parser.parse("2022-03-07T00:00:00Z")
+    end = parser.parse("2022-03-25T00:00:00Z")
+
+    while start < end:
+        print(start)
+        final_data += get_fx_calendar(start)
+        start = start + dt.timedelta(days=7)
+        time.sleep(1)
+
+    print(pd.DataFrame.from_dict(final_data))    

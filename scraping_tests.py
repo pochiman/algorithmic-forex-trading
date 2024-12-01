2bf373047baa59f8053eba8557034dd3d2396c35
pochiman <pochiman@yahoo.com>
2024-11-30 22:12:16 -0500

    initial commit

--- scraping_tests.py
+++ scraping_tests.py
@@ -1,11 +1,11 @@
-from scraping.investing_com import investing_com
 from scraping.dailyfx_com import dailyfx_com
+from scraping.investing_com import investing_com
 from scraping.bloomberg_com import bloomberg_com
 from scraping.fx_calendar import fx_calendar
 
 if __name__ == "__main__":
-    # print(investing_com())
-    # print(dailyfx_com())
-    # data = bloomberg_com()
-    # [print(x) for x in data]
+    print(dailyfx_com())
+    data = investing_com()
+    [print(x) for x in data]
+    print(bloomberg_com())
     print(fx_calendar())

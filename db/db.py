010891f9d08800a5124866fc10c5686f9063b799
pochiman <pochiman@yahoo.com>
2024-12-01 23:39:08 -0500

    initial commit

--- db/db.py
+++ db/db.py
@@ -12,3 +12,15 @@ class DataDB:
 
     def test_connection(self):
         print(self.db.list_collection_names())
+
+    def add_one(self, collection, ob):
+        try:
+            _ = self.db[collection].insert_one(ob)
+        except errors.InvalidOperation as error:
+            print("add_one error:", error)
+
+    def add_many(self, collection, list_ob):
+        try:
+            _ = self.db[collection].insert_many(list_ob)
+        except errors.InvalidOperation as error:
+            print("add_many error:", error)
--- main.py
+++ main.py
@@ -3,9 +3,16 @@ from infrastructure.instrument_collection import instrumentCollection
 from stream_example.streamer import run_streamer
 from db.db import DataDB
 
+
+def db_tests():
+    d = DataDB()
+
+    d.add_one(DataDB.SAMPLE_COLL, dict(age=12, name='paddy', street='elm'))
+
 if __name__ == '__main__':
     # api = OandaApi()
     # instrumentCollection.LoadInstruments("./data")
     # run_streamer()
-    d = DataDB()
-    d.test_connection()
+    # d = DataDB()
+    # d.test_connection()
+    db_tests()

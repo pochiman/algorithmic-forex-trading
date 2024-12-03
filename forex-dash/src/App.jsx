a074d12157b2495403db2ebee53a5b92ac685075
pochiman <pochiman@yahoo.com>
2024-12-03 17:57:50 -0500

    initial commit

--- forex-dash/src/App.jsx
+++ forex-dash/src/App.jsx
@@ -1,11 +1,28 @@
 import NavigationBar from "./components/NavigationBar";
+import {
+    BrowserRouter,
+    Route,
+    Routes
+}   from 'react-router-dom';
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   
     return (
-        <div id="app-holder">
-            <NavigationBar />
-        </div>
+        <>
+            <BrowserRouter>
+                <div id="app-holder">
+                    <NavigationBar />
+                    <div className="container">
+                        <Routes>
+                            <Route exact path="/" element={<Home />}/>
+                            <Route exact path="/dashboard" element={<Dashboard />}/>
+                        </Routes>
+                    </div>
+                </div>
+            </BrowserRouter>
+        </>    
     );
 }
 

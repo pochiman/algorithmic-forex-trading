dee005648aba9790bcc139ddb8119fa251c7fc41
pochiman <pochiman@yahoo.com>
2024-12-03 11:30:12 -0500

    initial commit

--- server.py
+++ server.py
@@ -1,12 +1,18 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 
+from scraping.bloomberg_com import bloomberg_com
+
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/api/test")
 def test():
     return jsonify(dict(message='hello'))
+
+@app.route("/api/headlines")
+def headlines():
+    return jsonify(bloomberg_com())
 
 
 if __name__ == "__main__":

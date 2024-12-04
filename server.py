5f1e6ee5dec513e1a7615cd103f6def0382ed6d2
pochiman <pochiman@yahoo.com>
2024-12-04 10:49:27 -0500

    initial commit

--- server.py
+++ server.py
@@ -1,12 +1,20 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-
 from api.oanda_api import OandaApi
+import http
+
 from scraping.bloomberg_com import bloomberg_com
 from scraping.investing_com import get_pair
 
 app = Flask(__name__)
 CORS(app)
+
+def get_response(data):
+    if data is None:
+        return jsonify(dict(message='error getting data')), http.HTTPStatus.NOT_FOUND
+    else:
+        return jsonify(data)
+
 
 @app.route("/api/test")
 def test():
@@ -15,25 +23,23 @@ def test():
 
 @app.route("/api/headlines")
 def headlines():
-    return jsonify(bloomberg_com())
+    return get_response(bloomberg_com())
+
+
+@app.route("/api/account")
+def account():
+    return get_response(OandaApi().get_account_summary())
 
 
 @app.route("/api/technicals/<pair>/<tf>")
 def technicals(pair, tf):
-    data = get_pair(pair, tf)
-    if data is None:
-        return jsonify(dict(message='error getting data'))
-    else:
-        return jsonify(data)
+    return get_response(get_pair(pair, tf))
     
 
 @app.route("/api/prices/<pair>/<granularity>/<count>")
 def prices(pair, granularity, count):
-    data = OandaApi().web_api_candles(pair, granularity, count)
-    if data is None:
-        return jsonify(dict(message='error getting data'))
-    else:
-        return jsonify(data)
+    return get_response(OandaApi().web_api_candles(pair, granularity, count))
+
 
 if __name__ == "__main__":
     app.run(debug=True)

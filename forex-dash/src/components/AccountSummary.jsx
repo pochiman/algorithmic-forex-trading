a28e570aaf8bea04ff50d9d8280a590c0cf8ba68
pochiman <pochiman@yahoo.com>
2024-12-04 11:03:32 -0500

    initial commit

--- forex-dash/src/components/AccountSummary.jsx
+++ forex-dash/src/components/AccountSummary.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import TitleHead from './TitleHead'
 
 function AccountSummary() {
+
+    const [count, setCount] = useState(0);
+
+    const handleIncr = () => {
+        setCount(count + 1);
+    }
+
     return (
         <div>
             <TitleHead title="Account Summary" />
+            <button onClick={() => handleIncr()}>Incr</button>
+            <div>{count}</div>
         </div>
     )
 }

92c1c0e50af0b034a613a048f1f6a0048bba6881
pochiman <pochiman@yahoo.com>
2024-12-04 23:22:43 -0500

    initial commit

--- forex-dash/src/app/chart.js
+++ forex-dash/src/app/chart.js
@@ -10,15 +10,26 @@ export function drawChart(chartData, p, g, divName) {
         open: chartData.mid_o,
         type: 'candlestick',
         xaxis: 'x',
-        yaxis: 'y'
-    }
+        yaxis: 'y',
+        increasing: { line: { width: 1, color: '#24A06B' }, fillcolor: '#24A06B' },
+        decreasing: { line: { width: 1, color: '#CC2E3C' }, fillcolor: '#CC2E3C' }
+    };
 
     let data = [trace];
 
     let layout = {
         title: `Data for ${p} ${g}`,
         height: '100%',
-        autosize: true
+        autosize: true,
+        showLegend: false,
+        margin: {
+            l: 50, r: 50, b: 50, t: 50
+        },
+        xaxis: {
+            rangeslider: {
+                visible: false
+            }
+        }
     };
 
     Plotly.newPlot(divName, data, layout, { responsive: true });
